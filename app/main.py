a6e0abf1b25cc69013e4ad93e399d81dd9e9bb22
jakottelaar <jarno.kottelaar@gmail.com>
2024-11-16 15:43:41 +0100

    Created database alive check before starting application

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
+import sys
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from app.api import router as api_router
+from app.core.database import wait_for_database
 from app.core.error_handler import validation_exception_handler
 from app.middleware.clerk_auth import VerifyTokenMiddleware
+from app.core.config import settings
+import logging
 
-app = FastAPI()
-
-app.include_router(api_router)
-
-# app.add_middleware(VerifyTokenMiddleware)
-
-app.add_exception_handler(RequestValidationError, validation_exception_handler)
+logger = logging.getLogger(__name__)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def start_application() -> FastAPI:
+
+    if not wait_for_database():
+        logger.critical("Could not connect to database. Exiting...")
+        sys.exit(1)
+
+    app = FastAPI()
+    app.include_router(api_router)
+    app.add_middleware(VerifyTokenMiddleware)
+    app.add_exception_handler(RequestValidationError, validation_exception_handler)
+    return app
+
+
+app = start_application()
