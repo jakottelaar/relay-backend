12c5576328cab989d9da470e499c1688d8e218b9
jakottelaar <jarno.kottelaar@gmail.com>
2025-10-11 22:59:17 +0200

    Updated go version and added local dev env for supabase

---
+++ supabase/migrations/20251011203654_create_profiles.sql
@@ -0,0 +1,51 @@
+create table profiles (
+    id uuid references auth.users not null primary key,
+    username text unique,
+    avatar_url text,
+    updated_at timestamp with time zone default now(),
+    created_at timestamp with time zone default now()
+
+    constraint profiles_username_check check (char_length(username) >= 3 and char_length(username) <= 30)
+);
+
+alter table profiles
+    enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles
+  for select using (true);
+
+create policy "Users can insert their own profile." on profiles
+  for insert with check ((select auth.uid()) = id);
+
+create policy "Users can update own profile." on profiles
+  for update using ((select auth.uid()) = id);
+
+create function public.handle_new_user()
+returns trigger
+set search_path = ''
+as $$
+begin
+  insert into profiles (id, username, avatar_url)   
+  values (
+    new.id, 
+    new.raw_user_meta_data->>'username', 
+    null
+  );
+  return new;
+end;
+$$ language plpgsql security definer;
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+insert into storage.buckets (id, name)
+  values ('avatars', 'avatars');
+
+create policy "Avatar images are publicly accessible." on storage.objects
+  for select using (bucket_id = 'avatars');
+
+create policy "Anyone can upload an avatar." on storage.objects
+  for insert with check (bucket_id = 'avatars');
+
+create policy "Anyone can update their own avatar." on storage.objects
+  for update using ((select auth.uid()) = owner) with check (bucket_id = 'avatars');
